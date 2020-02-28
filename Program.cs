c9d26cf5af92bb15d6950bfb10f3ced2d5d428cc
Jieun Kang <jieunkang101@gmail.com>
2020-02-28 15:35:07 -0800

    add methods to display pastry menu and order status.

--- Program.cs
+++ Program.cs
@@ -25,22 +25,21 @@ namespace Bakery
     |_____|  [___]'.__.'[___]   [___]    '.__.' [\__) ) |_______/ \'-;__/[__|  \_]'.__.'[___]  [\_:  /  
                                                                                                 \__.'   
       ");
-      Console.WriteLine("\n--------- Welcome to Pierre's Bakery -----------\n");
+      Console.WriteLine("\n----------- Welcome to Pierre's Bakery -----------\n");
       Console.WriteLine("Bread: $5    Special Deal: Buy 2, get 1 free \n");
       Console.WriteLine("Pastry: $2   Special Deal: 3 for $5\n");
-      Console.WriteLine("------------------------------------------------\n");
-      Console.Write("Press any key to continue");
+      Console.WriteLine("----------------------------------------------------\n");
+      Console.Write("Press any key to continue.\n");
       Console.ReadKey();
     }
 
     public static void MainMenu()
     {
-      Console.Clear();
-      Console.WriteLine("\n--------- Main Menu ----------");
+      Console.WriteLine("\n-------------------- Main Menu ---------------------");
       Console.WriteLine("Enter 'B' to view Bread Menu");
       Console.WriteLine("Enter 'P' to view Pastry Menu");
       Console.WriteLine("Enter 'V' to View Order Status");
-      Console.WriteLine("-------------------------------\n");
+      Console.WriteLine("----------------------------------------------------\n");
       string mainInput = Console.ReadLine();
       if ( mainInput == "B" || mainInput =="b")
       {
@@ -56,7 +55,7 @@ namespace Bakery
       }
       else 
       {
-        Console.WriteLine("Please enter B, P or V");
+        Console.WriteLine("Please enter B, P or V.\n");
         MainMenu();
       }
     }
@@ -64,29 +63,29 @@ namespace Bakery
     public static void BreadMenu()
     {
       Console.Clear();
-      Console.WriteLine("\n--------- Bread Menu --------");
+      Console.WriteLine("\n------------------- Bread Menu --------------------");
 
       for (int i = 0; i < Bread.Types.Length; i++)
       {
           Console.WriteLine($"{i+1}: {Bread.Types[i]}");
       }
 
-      Console.WriteLine("----------------------------------------\n"); 
-      Console.Write("Enter a number of bread to order: ");
+      Console.WriteLine("---------------------------------------------------\n"); 
+      Console.Write("Enter the Bread Number to order: ");
       int typeNumber = int.Parse(Console.ReadLine());
       Console.Write("Enter a number of loaves to add your order: ");
       int quantity = int.Parse(Console.ReadLine());
       Bread newBread = new Bread(typeNumber, quantity);
       showBreadOrder();
-      Console.WriteLine("\n-------------------------------------------\n"); 
-      Console.WriteLine("Would you like to add more bread? [Y or N]");
+      Console.WriteLine("\n-------------------------------------------------\n"); 
+      Console.WriteLine("Would you like to add more Bread? [Y or N]");
       string addBread = Console.ReadLine();
 
       if (addBread == "Y" || addBread == "y")
       {
         BreadMenu();
       }
-      else if (addBread == "N" || addBread == "n")
+      else 
       {
         MainMenu();
       }
@@ -100,9 +99,41 @@ namespace Bakery
     }
     public static void PastryMenu()
     {
-      
-    }
+      Console.Clear();
+      Console.WriteLine("\n------------------ Pastry Menu --------------------");
 
+      for (int i = 0; i < Pastry.Types.Length; i++)
+      {
+          Console.WriteLine($"{i+1}: {Pastry.Types[i]}");
+      }
+
+      Console.WriteLine("---------------------------------------------------\n"); 
+      Console.Write("Enter the Pastry Number to order: ");
+      int typeNumber = int.Parse(Console.ReadLine());
+      Console.Write("Enter a number of loaves to add your order: ");
+      int quantity = int.Parse(Console.ReadLine());
+      Pastry newPastry = new Pastry(typeNumber, quantity);
+      showPastryOrder();
+      Console.WriteLine("\n-------------------------------------------------\n"); 
+      Console.WriteLine("Would you like to add more Pastry? [Y or N]");
+      string addPastry = Console.ReadLine();
+
+      if (addPastry == "Y" || addPastry == "y")
+      {
+        PastryMenu();
+      }
+      else 
+      {
+        MainMenu();
+      }
+    }
+    public static void showPastryOrder()
+    {
+      foreach (Pastry pastryOrder in Pastry.PastryList)
+      {
+        Console.WriteLine($"\n Added {pastryOrder.Quantity} loaves of {pastryOrder.Type} to your order.");
+      }
+    }
     public static void OrderStatus()
     {
       
